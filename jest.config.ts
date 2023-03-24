16552753860cbe3defc0bf86cb65c176263ec0d9
lsndr <alexander@lsndr.me>
2023-03-24 11:34:27 +0400

    chore: use ts-jest

--- jest.config.ts
+++ jest.config.ts
@@ -2,23 +2,6 @@
 import { Config } from 'jest';
 
 export default {
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    '^.+\\.(t|j)s$': [
-      '@swc/jest',
-      {
-        jsc: {
-          parser: {
-            syntax: 'typescript',
-            decorators: true,
-          },
-          keepClassNames: true,
-          transform: {
-            legacyDecorator: true,
-            decoratorMetadata: true,
-          },
-        },
-      },
-    ],
-  },
 } satisfies Config;

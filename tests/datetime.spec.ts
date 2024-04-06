8e96548ed3e7f625c7735354d9617e0a94e963a6
Tom Quist <tom@ashbyhq.com>
2024-04-06 12:01:17 +0200

    feat: add tests for RRuleDateTime

---
+++ tests/datetime.spec.ts
@@ -0,0 +1,32 @@
+import { RRuleDateTime } from '../dist';
+
+test.each([new Date(873205200000), 873205200000])(
+  'Can create an RRuleDateTime from a %s',
+  (input) => {
+    const rruleDateTime = new RRuleDateTime(input, 'US/Eastern');
+
+    expect(rruleDateTime.toDate()).toEqual(new Date(873205200000));
+    expect(rruleDateTime.timestamp).toBe(873205200000);
+    expect(rruleDateTime.timezone.isLocal).toBe(false);
+    expect(rruleDateTime.timezone.name).toBe('US/Eastern');
+    expect(rruleDateTime.year).toBe(1997);
+    expect(rruleDateTime.month).toBe(9);
+    expect(rruleDateTime.day).toBe(2);
+    expect(rruleDateTime.hour).toBe(9);
+    expect(rruleDateTime.minute).toBe(0);
+    expect(rruleDateTime.second).toBe(0);
+    expect(rruleDateTime.millisecond).toBe(0);
+  },
+);
+
+it('Can create a local RRuleDateTime', () => {
+  const rruleDateTime = new RRuleDateTime(873205200000);
+  const props = [];
+  for (const prop in rruleDateTime) {
+    props.push(prop);
+  }
+
+  expect(rruleDateTime.timestamp).toBe(873205200000);
+  expect(rruleDateTime.timezone.isLocal).toBe(true);
+  expect(rruleDateTime.timezone.name).toBe('Local');
+});

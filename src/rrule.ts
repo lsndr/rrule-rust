c99cd5b3a14dd11aaaa84e5d4a76a31feffd01b5
Alexander <alexander@lsndr.me>
2024-06-08 15:48:52 +0700

    fix: count months from 1 (#175)

--- src/rrule.ts
+++ src/rrule.ts
@@ -23,18 +23,18 @@ export enum Frequency {
 }
 
 export enum Month {
-  January = 0,
-  February = 1,
-  March = 2,
-  April = 3,
-  May = 4,
-  June = 5,
-  July = 6,
-  August = 7,
-  September = 8,
-  October = 9,
-  November = 10,
-  December = 11,
+  January = 1,
+  February = 2,
+  March = 3,
+  April = 4,
+  May = 5,
+  June = 6,
+  July = 7,
+  August = 8,
+  September = 9,
+  October = 10,
+  November = 11,
+  December = 12,
 }
 
 export enum Weekday {

b1ed927259530036746c8807304e080ca7ae0723
Alexander <lsndrme@ya.ru>
2024-06-08 12:15:44 +0400

    fix: count months from 1

--- lib/js/month.rs
+++ lib/js/month.rs
@@ -3,18 +3,18 @@ use napi_derive::napi;
 
 #[napi(js_name = "Month")]
 pub enum Month {
-  January,
-  February,
-  March,
-  April,
-  May,
-  June,
-  July,
-  August,
-  September,
-  October,
-  November,
-  December,
+  January = 1,
+  February = 2,
+  March = 3,
+  April = 4,
+  May = 5,
+  June = 6,
+  July = 7,
+  August = 8,
+  September = 9,
+  October = 10,
+  November = 11,
+  December = 12,
 }
 
 impl Into<month::Month> for Month {
--- lib/rrule/month.rs
+++ lib/rrule/month.rs
@@ -57,26 +57,6 @@ impl FromStr for Month {
   }
 }
 
-impl From<&u8> for Month {
-  fn from(month: &u8) -> Self {
-    match month {
-      1 => Month::January,
-      2 => Month::February,
-      3 => Month::March,
-      4 => Month::April,
-      5 => Month::May,
-      6 => Month::June,
-      7 => Month::July,
-      8 => Month::August,
-      9 => Month::September,
-      10 => Month::October,
-      11 => Month::November,
-      12 => Month::December,
-      _ => panic!("Invalid month number: {}", month),
-    }
-  }
-}
-
 impl Into<chrono::Month> for &Month {
   fn into(self) -> chrono::Month {
     match self {

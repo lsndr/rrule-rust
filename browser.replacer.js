999934b4b47e72f40cad24cf04a6e37285355ca1
Alexander <alexander@lsndr.me>
2024-10-03 22:15:28 +0700

    fix: publish fixed wasm runtime (#305)

--- browser.replacer.js
+++ browser.replacer.js
@@ -1,7 +1,16 @@
 module.exports.default = ({ orig, file }) => {
-  if (!file.includes('dist/browser') || orig !== "from '@lib'") {
+  if (!file.includes('dist/browser')) {
     return orig;
   }
 
-  return "from '@rrule-rust/lib-wasm32-wasi'";
+  switch (orig) {
+    case "from '@lib'":
+      return "from '@rrule-rust/lib-wasm32-wasi'";
+    case "require('@napi-rs/wasm-runtime')":
+      return "frequire('./runtime.js')";
+    case "from '@napi-rs/wasm-runtime'":
+      return "from './runtime.js'";
+    default:
+      return orig;
+  }
 };

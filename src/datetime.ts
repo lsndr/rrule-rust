bdd1b49701d853aeab8a129b376edf760b64ef32
lsndr <lsndrme@ya.ru>
2025-12-09 10:18:37 +0400

    style: fix

--- src/datetime.ts
+++ src/datetime.ts
@@ -525,11 +525,9 @@ export class DateTime<T extends Time | undefined> {
    * ```
    */
   public toPlain(
-    // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents -- Needed for proper overload resolution
     options: ToPlainDateTimeOptions & { stripUtc: false },
   ): T extends Time ? DateTimeLike : DateLike;
   public toPlain(
-    // eslint-disable-next-line @typescript-eslint/no-redundant-type-constituents -- Needed for proper overload resolution
     options: ToPlainDateTimeOptions & { stripUtc: true },
   ): T extends Time ? StripUtc<DateTimeLike> : DateLike;
   public toPlain(): T extends Time ? DateTimeLike : DateLike;
